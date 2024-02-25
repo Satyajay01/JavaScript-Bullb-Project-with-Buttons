85f5d629397cde7b599319b9d29d4c422dcc6528
satyajay01 <satyajaydibya0@gmail.com>
2024-02-25 19:02:41 +0530

    Bulb on off with button complete project

---
+++ script.js
@@ -0,0 +1,18 @@
+function bulb_on() {
+    const bulb_off_img =document.querySelector("#off");
+    bulb_off_img.style.display="none";
+
+    const bulb_on_img=document.querySelector("#on");
+    bulb_on_img.style.display="block";
+
+}
+
+
+function bulb_off() {
+    const bulb_off_img =document.querySelector("#off");
+    bulb_off_img.style.display="block";
+
+    const bulb_on_img=document.querySelector("#on");
+    bulb_on_img.style.display="none";
+
+}
